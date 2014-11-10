684750ec7d4b8b88556b174541691365de746513
Kevin Stewart <krstewar@ucsc.edu>
2014-11-10 14:47:47 -0800

    Removing unnecessary project files

--- web2py/web2py.app/Contents/Resources/applications/Notebook/models/menu.py
+++
@@ -1,6 +0,0 @@
-response.title = request.application
-response.subtitle = 'Personal Note Storage'
-response.meta.author = 'you'
-response.meta.description = 'A personal note taking application'
-response.meta.keywords = 'bla bla bla'
-response.menu = [ [ 'Notebook', False, URL('index') ] ]
--- web2py/web2py.app/Contents/Resources/applications/images/models/menu.py
+++
@@ -1,6 +0,0 @@
-response.title = request.application
-response.subtitle = 'customize me!'
-response.meta.author = 'you'
-response.meta.description = 'describe your app'
-response.meta.keywords = 'bla bla bla'
-response.menu = [ [ 'Index', False, URL('index') ] ]
